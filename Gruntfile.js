3302225d5c49e0b2d238db461ac694d78eb3ef30
konstantin.krassmann <kkonstantin@gmx.de>
2014-04-11 16:04:58 +0200

    Added Grunt support.

---
+++ Gruntfile.js
@@ -0,0 +1,17 @@
+module.exports = function (grunt)
+{
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    qunit: {
+      all:["test/*.html"]
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+
+
+  grunt.registerTask('test', ['qunit']);
+};
