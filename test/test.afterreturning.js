0234811690ea743ea7954d0fe693250a64c07b0c
anton <anton.al.ivanov@gmail.com>
2014-04-15 22:44:34 +0200

    Formatted the tests, minor changes to the added tests

--- test/test.afterreturning.js
+++ test/test.afterreturning.js
@@ -1,66 +1,63 @@
 module("jsAspect.afterReturning");
 
 (function() {
-    
-    test("jsAspect.inject: 'afterReturning' advice, 'prototypeMethods' pointcut", function() {
-        function Object() {
-        }
-        Object.prototype.wrapValue = function(value) {
-            return {value: value};
-        };
-        
-        jsAspect.inject(Object, jsAspect.POINTCUT.PROTOTYPE_METHODS, jsAspect.JOIN_POINT.AFTER_RETURNING,
-            function afterReturningCallback(context, retValue) {
-                retValue.value = retValue.value + 1;
-                return retValue;
-            }
-        );
 
-        var obj = new Object();
-        
-        equal(obj.wrapValue(2).value, 3, "'afterReturning' advice is applied");     
-    });
+  test("jsAspect.inject: 'afterReturning' advice, 'prototypeMethods' pointcut", function() {
+    function Target() {
+    }
 
-    test("jsAspect.inject: several 'afterReturning' aspects", function() {
-        function Object() {
-        }
+    Target.prototype.wrapValue = function(value) {
+      return {value: value};
+    };
 
-        Object.prototype.identity = function(value) {
-            return value;
-        };
-        
-        ["aspect1", "aspect2", "aspect3"].forEach(function (aspectName) {
-            jsAspect.inject(Object, jsAspect.POINTCUT.PROTOTYPE_METHODS, jsAspect.JOIN_POINT.AFTER_RETURNING,
-                function afterReturningCallback(context, retValue) {
-                    return retValue + "_" + aspectName;
-                }
-            );
-        });
-        
-        equal(new Object().identity("value"), "value_aspect3_aspect2_aspect1", "'afterReturning' several aspects applied in the reverse order");
-    });
-    test("jsAspect.inject: 'afterReturning' has context", function ()
-    {
-      function Object()
-      {
+    jsAspect.inject(Target, jsAspect.POINTCUT.PROTOTYPE_METHODS, jsAspect.JOIN_POINT.AFTER_RETURNING,
+      function afterReturningCallback(context, retValue) {
+        retValue.value = retValue.value + 1;
+        return retValue;
       }
+    );
 
-      Object.prototype.identity = function (value)
-      {
-        return value;
-      };
+    var obj = new Target();
 
-        jsAspect.inject(Object, jsAspect.POINTCUT.PROTOTYPE_METHODS, jsAspect.JOIN_POINT.AFTER_RETURNING,
-          function afterReturningCallback(context, retValue)
-          {
-            equal(context.method.name, "identity", "method name is passed to 'context' properly");
-            deepEqual(context.method.arguments, ["test"], "method arguments are passed to 'context' properly");
-            deepEqual(retValue, "test", "return value still passed.");
-          }
-        );
+    equal(obj.wrapValue(2).value, 3, "'afterReturning' advice is applied");
+  });
 
-      var obj1 = new Object();
-      obj1.identity("test");
+  test("jsAspect.inject: several 'afterReturning' aspects", function() {
+    function Target() {
+    }
 
+    Target.prototype.identity = function(value) {
+      return value;
+    };
+
+    ["aspect1", "aspect2", "aspect3"].forEach(function (aspectName) {
+      jsAspect.inject(Target, jsAspect.POINTCUT.PROTOTYPE_METHODS, jsAspect.JOIN_POINT.AFTER_RETURNING,
+        function afterReturningCallback(context, retValue) {
+          return retValue + "_" + aspectName;
+        }
+      );
     });
+
+    equal(new Target().identity("value"), "value_aspect3_aspect2_aspect1", "'afterReturning' several aspects applied in the reverse order");
+  });
+
+  test("jsAspect.inject: 'afterReturning' has context", function() {
+    function Target() {
+    }
+
+    Target.prototype.square = function(num) {
+      return num * num;
+    };
+
+    jsAspect.inject(Target, jsAspect.POINTCUT.PROTOTYPE_METHODS, jsAspect.JOIN_POINT.AFTER_RETURNING, 
+      function afterReturningCallback(context, retValue) {
+        equal(context.method.name, "square", "method name is passed to 'context' properly");
+        deepEqual(context.method.arguments, [3], "method arguments are passed to 'context' properly");
+        deepEqual(retValue, 9, "return value is passed");
+      }
+    );
+
+    var obj = new Target();
+    obj.square(3);
+  });
 })();
--- test/test.afterthrowing.js
+++ test/test.afterthrowing.js
@@ -1,131 +1,130 @@
 module("jsAspect.afterThrowing");
 
 (function() {
-    
-    test("jsAspect.inject: 'afterThrowing' advice, 'prototypeMethods' pointcut", function() {
-        function Object() {
-        }
-        Object.prototype.method1 = function() {
-            throw new Error("method1exception");
-        };
-        
-        Object.prototype.method2 = function() {
-            throw new Error("method2exception");
-        };
-        
-        jsAspect.inject(Object, jsAspect.POINTCUT.PROTOTYPE_METHODS, jsAspect.JOIN_POINT.AFTER_THROWING,
-            function afterThrowingCallback(context, exception) {
-                this.thrownExceptions = this.thrownExceptions || [];
-                this.thrownExceptions.push(exception.message);
-            }
-        );
 
-        var obj = new Object();
-        var thrownExceptions = [];
-        
-        ["method1", "method2"].forEach(function (methodName) {
-            try {
-                obj[methodName]();
-            } catch (exception) {
-                thrownExceptions.push(exception.message);
-            }
-        });
-        
-        deepEqual(thrownExceptions, ["method1exception", "method2exception"], "Exceptions are thrown from the methods");
-        deepEqual(obj.thrownExceptions, ["method1exception", "method2exception"], "Aspects recieve the exceptions");
-    });
-    
-    test("jsAspect.inject: 'afterThrowing' several aspects", function() {
-        function Object() {
-        }
-
-        Object.prototype.method1 = function() {
-            throw new Error("method1exception");
-        };
-        
-        Object.prototype.method2 = function() {
-            throw new Error("method2exception");
-        };
-        
-        jsAspect.inject(Object, jsAspect.POINTCUT.PROTOTYPE_METHODS, jsAspect.JOIN_POINT.AFTER_THROWING,
-            function afterThrowingCallback(context, exception) {
-                exception.message = exception.message + "_aspect1"
-            }
-        );
-        jsAspect.inject(Object, jsAspect.POINTCUT.PROTOTYPE_METHODS, jsAspect.JOIN_POINT.AFTER_THROWING,
-            function afterThrowingCallback(context, exception) {
-                exception.message = exception.message + "_aspect2"
-            }
-        );
-
-        var obj = new Object();
-        var thrownExceptions = [];
-        
-        ["method1", "method2"].forEach(function (methodName) {
-            try {
-                obj[methodName]();
-            } catch (exception) {
-                thrownExceptions.push(exception.message);
-            }
-        });
-        
-        deepEqual(thrownExceptions, ["method1exception_aspect2_aspect1", "method2exception_aspect2_aspect1"], "Multiple aspects are applied");
-    });
-
-    test("jsAspect.inject: 'afterThrowing' advice throws an exception", function() {
-        function Object() {
-        }
-        
-        Object.prototype.method = function() {
-            throw new Error("method1exception");
-        };
-        
-        jsAspect.inject(Object, jsAspect.POINTCUT.PROTOTYPE_METHODS, jsAspect.JOIN_POINT.AFTER_THROWING,
-            function afterThrowingCallback(context, exception) {
-                throw new Error("callbackexception");
-            }
-        );
-
-        var obj = new Object();
-        
-        try {
-            obj.method();
-            ok(false, "Exception should have been thrown at this point");
-        } catch (e) {
-            equal(e.message, "callbackexception", "Exception from advice");
-        }
-    });
-
-  test("jsAspect.inject: 'afterThrowing' advice has context", function ()
-  {
-    function Object()
-    {
+  test("jsAspect.inject: 'afterThrowing' advice, 'prototypeMethods' pointcut", function() {
+    function Target() {
     }
 
-    Object.prototype.method = function ()
-    {
+    Target.prototype.method1 = function() {
       throw new Error("method1exception");
     };
 
-    jsAspect.inject(Object, jsAspect.POINTCUT.PROTOTYPE_METHODS, jsAspect.JOIN_POINT.AFTER_THROWING,
-      function afterThrowingCallback(context, exception)
-      {
-        equal(context.method.name, "method", "method name is passed to 'context' properly");
-        deepEqual(context.method.arguments, [], "method arguments are passed to 'context' properly");
+    Target.prototype.method2 = function() {
+      throw new Error("method2exception");
+    };
+
+    jsAspect.inject(Target, jsAspect.POINTCUT.PROTOTYPE_METHODS, jsAspect.JOIN_POINT.AFTER_THROWING,
+      function afterThrowingCallback(context, exception) {
+        this.thrownExceptions = this.thrownExceptions || [];
+        this.thrownExceptions.push(exception.message);
+      }
+    );
+
+    var obj = new Target();
+    var thrownExceptions = [];
+
+    ["method1", "method2"].forEach(function (methodName) {
+      try {
+        obj[methodName]();
+      } catch (exception) {
+        thrownExceptions.push(exception.message);
+      }
+    });
+
+    deepEqual(thrownExceptions, ["method1exception", "method2exception"], "Exceptions are thrown from the methods");
+    deepEqual(obj.thrownExceptions, ["method1exception", "method2exception"], "Aspects recieve the exceptions");
+  });
+
+  test("jsAspect.inject: 'afterThrowing' several aspects", function() {
+    function Target() {
+    }
+
+    Target.prototype.method1 = function() {
+      throw new Error("method1exception");
+    };
+
+    Target.prototype.method2 = function() {
+      throw new Error("method2exception");
+    };
+
+    jsAspect.inject(Target, jsAspect.POINTCUT.PROTOTYPE_METHODS, jsAspect.JOIN_POINT.AFTER_THROWING,
+      function afterThrowingCallback(context, exception) {
+        exception.message = exception.message + "_aspect1"
+      }
+    );
+    jsAspect.inject(Target, jsAspect.POINTCUT.PROTOTYPE_METHODS, jsAspect.JOIN_POINT.AFTER_THROWING,
+      function afterThrowingCallback(context, exception) {
+        exception.message = exception.message + "_aspect2"
+      }
+    );
+
+    var obj = new Target();
+    var thrownExceptions = [];
+
+    ["method1", "method2"].forEach(function (methodName) {
+      try {
+        obj[methodName]();
+      } catch (exception) {
+        thrownExceptions.push(exception.message);
+      }
+    });
+
+    deepEqual(thrownExceptions, ["method1exception_aspect2_aspect1", "method2exception_aspect2_aspect1"], "Multiple aspects are applied");
+  });
+
+  test("jsAspect.inject: 'afterThrowing' advice throws an exception", function() {
+    function Target() {
+    }
+
+    Target.prototype.method = function() {
+      throw new Error("method1exception");
+    };
+
+    jsAspect.inject(Target, jsAspect.POINTCUT.PROTOTYPE_METHODS, jsAspect.JOIN_POINT.AFTER_THROWING,
+      function afterThrowingCallback(context, exception) {
         throw new Error("callbackexception");
       }
     );
 
-    var obj = new Object();
+    var obj = new Target();
 
-    try
-    {
+    try {
       obj.method();
       ok(false, "Exception should have been thrown at this point");
-    }
-    catch (e)
-    {
+    } catch (e) {
       equal(e.message, "callbackexception", "Exception from advice");
+    }
+  });
+
+  test("jsAspect.inject: 'afterThrowing' advice has context", function() {
+    var args = ["arg1", "arg2", "arg3"];
+
+    var exceptionMessagesInAdvice = [];
+
+    function Target() {
+    }
+
+    Target.prototype.method = function() {
+      throw new Error("method1exception");
+    };
+
+    jsAspect.inject(Target, jsAspect.POINTCUT.PROTOTYPE_METHODS, jsAspect.JOIN_POINT.AFTER_THROWING,
+      function afterThrowingCallback(context, exception) {
+        equal(context.method.name, "method", "method name is passed to 'context' properly");
+        deepEqual(context.method.arguments, args, "method arguments are passed to 'context' properly");
+        exceptionMessagesInAdvice.push(exception.message);
+      }
+    );
+
+    var obj = new Target();
+
+    try {
+      obj.method.apply(obj, args);
+      ok(false, "Exception should have been thrown at this point");
+    } catch (e) {
+      equal(e.message, "method1exception", "Exception caught");
+      deepEqual(exceptionMessagesInAdvice, ["method1exception"], "Advice is applied");
     }
   });
 })();
